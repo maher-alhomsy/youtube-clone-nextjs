add2afe2b0f2b31dd70d64fdf7d9db6e4991b7a2
Maher Alhoumsi <maher.alhoumsi@gmail.com>
2026-03-17 22:16:12 +0300

    feat: add UI components for input, separator, sheet, sidebar, skeleton, and tooltip
    - Implemented Input component for form inputs with customizable styles.
    - Created Separator component for visual separation in UI.
    - Developed Sheet component for modal-like overlays with customizable content.
    - Introduced Sidebar component with collapsible functionality and mobile support.
    - Added Skeleton component for loading states.
    - Implemented Tooltip component for displaying additional information on hover.
    - Integrated hooks for mobile detection and sidebar state management.
    - Added AuthButton and HomeNavbar components for user authentication and navigation.
    - Created HomeSidebar with main and personal sections for user-specific navigation.
    - Established HomeLayout to structure the home page with sidebar and navbar.

---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Link from 'next/link';
+
+import {
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarGroupContent,
+} from '@/components/ui/sidebar';
+import { FlameIcon, HomeIcon, PlaySquareIcon } from 'lucide-react';
+
+const itemes = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: HomeIcon,
+  },
+  {
+    title: 'Subscribions',
+    url: '/feed/subscriptions',
+    icon: PlaySquareIcon,
+    auth: true,
+  },
+  {
+    title: 'Trending',
+    url: '/feed/trending',
+    icon: FlameIcon,
+  },
+];
+
+export const MainSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {itemes.map((item) => (
+            <SidebarMenuItem key={item.url}>
+              <SidebarMenuButton
+                asChild
+                isActive={false}
+                onClick={() => {}}
+                tooltip={item.title}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
