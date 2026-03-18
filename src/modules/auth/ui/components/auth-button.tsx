9ebcf37a764b917f294d5023f664d0bb7c4670c5
Maher Alhoumsi <maher.alhoumsi@gmail.com>
2026-03-18 13:13:16 +0300

    feat: integrate Clerk for authentication and add protected routes

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -2,14 +2,26 @@ import { UserCircleIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 
+import { SignInButton, UserButton, SignOutButton, Show } from '@clerk/nextjs';
+
 export const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <Show when="signed-out">
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </Show>
+
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+    </>
   );
 };
