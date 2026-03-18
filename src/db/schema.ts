0bd44f8de32586d604289e310f7f1c99e7660080
Maher Alhoumsi <maher.alhoumsi@gmail.com>
2026-03-18 14:33:19 +0300

    feat: add drizzle configuration and database schema
    - Created drizzle.config.ts for database configuration using dotenv.
    - Updated package.json to include drizzle-orm, drizzle-kit, dotenv, and @neondatabase/serverless.
    - Added src/db/index.ts to initialize the database connection with drizzle-orm.
    - Defined users table schema in src/db/schema.ts with necessary fields and constraints.

---
+++ src/db/schema.ts
@@ -0,0 +1,23 @@
+import {
+  pgTable,
+  integer,
+  varchar,
+  uuid,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    clerkId: text('clerk_id').notNull().unique(),
+    name: text('name').notNull(),
+    imageUrl: text('image_url').notNull(),
+
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  },
+  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+);
