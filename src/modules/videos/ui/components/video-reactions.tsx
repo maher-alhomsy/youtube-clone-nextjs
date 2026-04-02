9571ef57bfb8a4dae697da44f53d19c7d1848c70
Maher Alhoumsi <maher.alhoumsi@gmail.com>
2026-04-02 19:42:22 +0300

    feat: implement video reactions feature with like and dislike functionality

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -1,34 +1,100 @@
+import { toast } from 'sonner';
+import { useClerk } from '@clerk/nextjs';
 import { ThumbsDownIcon, ThumbsUpIcon } from 'lucide-react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { cn } from '@/lib/utils';
+import { useTRPC } from '@/trpc/client';
 import { Button } from '@/components/ui/button';
+import { type VideoGetOneOutput } from '../../types';
 import { Separator } from '@/components/ui/separator';
 
-export const VideoReactions = () => {
-  const viewerReaction = 'like';
+interface Props {
+  likes: number;
+  videoId: string;
+  dislikes: number;
+  viewerReaction: VideoGetOneOutput['viewerReaction'];
+}
+
+export const VideoReactions = ({
+  likes,
+  videoId,
+  dislikes,
+  viewerReaction,
+}: Props) => {
+  const trpc = useTRPC();
+  const clerk = useClerk();
+  const queryClient = useQueryClient();
+
+  const { mutate: likeMutation, isPending: isLikePending } = useMutation(
+    trpc.videoReactions.like.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(
+          trpc.videos.getOne.queryOptions({ id: videoId }),
+        );
+      },
+
+      onError: (error) => {
+        toast.error(error.message);
+
+        if (error.data?.code === 'UNAUTHORIZED') {
+          clerk.openSignIn();
+        }
+      },
+    }),
+  );
+
+  const { mutate: dislikeMutation, isPending: isDislikePending } = useMutation(
+    trpc.videoReactions.dislike.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(
+          trpc.videos.getOne.queryOptions({ id: videoId }),
+        );
+      },
+      onError: (error) => {
+        toast.error(error.message);
+
+        if (error.data?.code === 'UNAUTHORIZED') {
+          clerk.openSignIn();
+        }
+      },
+    }),
+  );
+
+  const handleLike = () => {
+    likeMutation({ videoId });
+  };
+
+  const handleDislike = () => {
+    dislikeMutation({ videoId });
+  };
 
   return (
     <div className="flex items-center flex-none">
       <Button
+        onClick={handleLike}
         variant="secondary"
+        disabled={isLikePending || isDislikePending}
         className="rounded-l-full rounded-r-none gap-2 pr-4 cursor-pointer"
       >
         <ThumbsUpIcon
           className={cn('size-5', viewerReaction === 'like' && 'fill-black')}
         />
-        {1}
+        {likes}
       </Button>
 
       <Separator orientation="vertical" className="h-7" />
 
       <Button
         variant="secondary"
+        onClick={handleDislike}
+        disabled={isLikePending || isDislikePending}
         className="rounded-l-none rounded-r-full pl-3 cursor-pointer"
       >
         <ThumbsDownIcon
-          className={cn('size-5', viewerReaction !== 'like' && 'fill-black')}
+          className={cn('size-5', viewerReaction === 'dislike' && 'fill-black')}
         />
-        {1}
+        {dislikes}
       </Button>
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,12 +3,14 @@ import { videosRouter } from '@/modules/videos/server/procedure';
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
+import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 
 export const appRouter = createTRPCRouter({
   videos: videosRouter,
   studio: studioRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 });
 
 // export type definition of API
